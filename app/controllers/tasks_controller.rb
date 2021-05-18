4bffbbfcc8893862f817d8517d960358f5bfb4c7
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-18 14:21:30 +0900

    add views, controller before-action, update schema

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,43 @@
 class TasksController < ApplicationController
+
+  def index
+    before_action :set_params, only [:show, :edit, :update, :destroy]
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render 'new.html.erb'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(params)
+      redirect_to task_path(@task)
+    else
+      render 'edit.html.erb'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end

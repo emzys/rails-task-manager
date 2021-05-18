e309cededde15fc590443a6d8e8300df3a73de9c
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-18 14:33:51 +0900

    add strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    before_action :set_params, only [:show, :edit, :update, :destroy]
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params)
+    @task = Task.new(task_params)
     if @task.save
       redirect_to task_path(@task)
     else
@@ -23,12 +22,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-    if @task.update(params)
+    if @task.update(task_params)
       redirect_to task_path(@task)
     else
       render 'edit.html.erb'
@@ -36,8 +33,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
